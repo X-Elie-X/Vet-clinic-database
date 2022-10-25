7e7083a85ebac0c61fa0317b73e449a344c13630
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-10-25 23:48:52 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -2,4 +2,4 @@ INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES('Agumon', date'2020-02-03','0','TRUE','10.23'),
 ('Gabumon', date'2018-11-15','2','TRUE','8'),
 ('Pikachu', date'2021-01-07','1','FALSE','15.04'),
-'Devimon', date'2017-05-12','5','TRUE','11');
+('Devimon', date'2017-05-12','5','TRUE','11');
