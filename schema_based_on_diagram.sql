2d2f2e21ea710a436e03a5d5fbc4ee60b27776e6
yasinabdmahmood <abd181103@gmail.com>
2022-11-01 16:49:13 +0300

    Add invoice_items , treatments and medical_histories tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -12,3 +12,26 @@ CREATE TABLE "invoices" (
   "medical_history__id" INT,
   PRIMARY KEY ("id")
 );
+
+CREATE TABLE "medical_histories" (
+  "id" INT,
+  "admitted_at" TIMESTAMP,
+  "patient_id" INT,
+  "status" VARCHAR,
+  PRIMARY KEY ("id")
+);
+CREATE TABLE "treatments" (
+  "id" INT,
+  "type" VARCHAR,
+  "name" VARCHAR,
+  PRIMARY KEY ("id")
+);
+CREATE TABLE "invoice_items" (
+  "id" INT,
+  "unit_price" DECIMAL,
+  "quantity" INT,
+  "total_price" DECIMAL,
+  "invoice_id" INT,
+  "treatment_id" INT,
+  PRIMARY KEY ("id")
+);
